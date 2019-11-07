5917cdf1ca106676937a14f8eee771b45116f677
MichaelRJohnson-code <mrj22@buffalo.com>
2019-11-07 10:54:55 -0500

    Phase 2 demoed not done.

---
+++ src/Game1/database/Database.scala
@@ -0,0 +1,10 @@
+package Game1.database
+
+trait Database {
+
+  def playerExists(username: String): Boolean
+  def createPlayer(username: String): Unit
+  def saveGameState(username: String, gameState: String): Unit
+  def loadGameState(username: String): String
+
+}
